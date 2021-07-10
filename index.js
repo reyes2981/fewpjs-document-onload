edb730ce9b90ad451cec807ac1b5141d8594259d
noe <noe.reyes92@yahoo.com>
2021-07-10 16:14:49 -0500

    commit tree 15280a924af42fee90f18040da8c9a71133ff7d5

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+
+    const textinput = document.getElementById('text');
+    textinput.innerHTML = "This is really cool!"
+  });
+
